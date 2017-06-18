adfacc4b9e0ce66b0e3f151c18492a24a819a10c
SamCiavardini <sam.ciavardini@gmail.com>
2017-06-17 21:32:36 -0600

    added new files

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var logic = require('./logic.js');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -12,6 +13,24 @@ app.set('view engine', 'ejs');
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
+
+app.get("/ups", function(request, response){
+	response.sendFile(__dirname + "/public/prove09.html");
+})
+
+app.get("/rates", function(request, response){
+
+	var mailType = request.query.mailType;
+	var weight = request.query.weight;
+	var price = logic.calculatePrice(weight, mailType);
+	var params = {
+		mailType: mailType,
+		weight: weight,
+		price: price
+	}
+	
+	response.render("results.ejs", params);
+})
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
---
+++ logic.js
@@ -0,0 +1,58 @@
+function calculatePrice(weight, type){
+	var prices;
+	switch (type) {
+		case 'stamp':
+			prices = {
+				1: 0.49,
+				2: 0.70,
+				3: 0.91,
+				3.5: 1.12
+			}
+		break;
+		case 'meter':
+			prices = {
+				1:   0.46,
+				2:   0.67,
+				3:   0.88,
+				3.5: 1.09
+			}
+		break;
+		case 'flat':
+			prices = {
+				1: 0.98,
+				2: 1.19,
+				3: 1.40,
+				4: 1.61,
+				5: 1.82,
+				6: 2.03,
+				7:  2.24,
+				8:  2.45,
+				9:  2.66,
+				10: 2.87,
+				11: 3.08,
+				12: 3.29,
+				13: 3.50
+			}
+		break;
+		case 'parcel':
+			prices = {
+				4:  2.67,
+				5:  2.85,
+				6:  3.03,
+				7:  3.21,
+				8:  3.39,
+				9:  3.57,
+				10: 3.75,
+				11: 3.93,
+				12: 4.11,
+				13: 4.29
+			}
+		break;
+	}
+	for (var key in prices){
+		if (weight < key)
+			return prices[key];
+	}
+}
+
+module.exports.calculatePrice = calculatePrice;
